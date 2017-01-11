986e600af1d6ae36c6468ec717e0d6f7fd2385db
Taylor Mason <taylorwmason@gmail.com>
2017-01-11 04:05:02 -0500

    Update calc.js
    Added check for operator var to prevent code injection

--- exercises/calc.js
+++ exercises/calc.js
@@ -1,14 +1,20 @@
 var readline = require('readline-sync');
 
-var num1 = readline.question("Please enter a number ");
-var num2 = readline.question("Please enter another number ");
+var num1 = readline.question("Number one? ");
+var num2 = readline.question("Number two? ");
 var operator = readline.question("Please enter an operator (+, - , / , * ) ");
 
 var operation = function(first_num, second_num, operator){
-  if(!isNaN(first_num) || !isNan(second_num)){
-    var result = eval(first_num + operator + second_num);
-    console.log(first_num + " " + operator + " " + second_num + " = " + result) 
-  }
+	if (!isNaN(num1) && !isNaN(num2)) {
+		if (operator == "+" || operator == "-" || operator == "*" || operator == "/" || operation== "%"){
+				var result = eval(first_num + operator + second_num);
+    			console.log(first_num + " " + operator + " " + second_num + " = " + result)
+		} else {
+				console.log("invalid operator")
+		}
+	;} else {
+		console.log("one of more of your numbers is invalid")
+	}
 };
 
-operation(num1, num2, operator);
+operation(num1,num2,operator);
